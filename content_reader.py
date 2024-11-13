26e4bc6cf2e789ae0ec177c654e71fa0d17360f4
wooginawunan <wooginawunan@gmail.com>
2024-11-13 12:21:01 -0500

    content_reader

--- content_reader.py
+++ content_reader.py
@@ -3,17 +3,16 @@ from pathlib import Path
 import os
 import fitz
 
-from utils import load_all_papers
+from utils import load_all_papers, get_first_n_tokens_tiktoken
 import ollama
 import concurrent.futures
 import pandas as pd
 import argparse
 from tqdm.rich import tqdm
+from datetime import datetime
 
 DEFAULT_DOC_PATH = Path('/Users/nanwu/Desktop/readings')
-
-ROOT_PATH = Path('./data')
-# %%
+ROOT_PATH = Path('/Users/nanwu/datasets/paper_manager/')
 
 def doc_type_classifier(aspect_ratios, words_per_page):
 
@@ -52,41 +51,84 @@ def analyze_pdf(file_path, n_pages_to_analyze=10):
     return doc_type, font_page_text
 
 
-def get_file_info(file_path, prompt, model='llama3.2:1b'):
-    doc_type, font_page_text = analyze_pdf(file_path, n_pages_to_analyze=5)
-    info = ollama.generate(model=model, prompt=prompt.format(font_page_text=font_page_text))['response']
-    return file_path, doc_type, info
-
-def main(doc_path: Path):
+def get_file_info(file_idx, context):
     model = 'llama3.2:1b'
     prompt = """
     Extract the title, authors and year published from the following text: {font_page_text}
 
-    You return text: [title. authors (if available). year_published (if available).]
+    Start your response with: title. authors (if available). year_published (if available).
     """
+    info = ollama.generate(model=model, prompt=prompt.format(font_page_text=context))['response']
+    return file_idx, info
 
-    files = load_all_papers(doc_path)[:3]
-    file_names = [file.name.strip('.pdf') for file in files]
-    file_dates = [os.path.getmtime(file) for file in files]
+    
+def prepare_file_metadata(doc_path: Path, save_path: Path, n_pages_to_analyze: int = 5, n_tokens_to_context: int = 100):
 
-    print(f"Analyzing {len(files)} files")
+    if not (save_path / 'paper_info.csv').exists():
+        files = load_all_papers(doc_path)
+        file_names = [file.name.strip('.pdf') for file in files]
+        file_dates = [datetime.fromtimestamp(os.path.getmtime(file)).date() for file in tqdm(files, desc="Getting file dates")]
+        file_types, font_page_texts = zip(*[analyze_pdf(file, n_pages_to_analyze=n_pages_to_analyze) \
+            for file in tqdm(files, desc="Analyzing files")])
+        file_contexts = [get_first_n_tokens_tiktoken(text, 'llama3.2:1b', n_tokens_to_context) \
+            for text in tqdm(font_page_texts, desc="Getting file contexts")]
+        print(f"Analyzing {len(files)} files")
 
-    file_metadata = pd.DataFrame({'file_path': files, 'file_name': file_names, 'file_date': file_dates})
-    print(file_metadata)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(get_file_info, file_path, prompt, model) for file_path in tqdm(files, desc="Submitting tasks")]
-        results = list(tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Processing results"))
+        file_metadata = pd.DataFrame({
+            'file_idx': range(len(files)),
+            'file_path': files, 
+            'file_name': file_names, 
+            'file_date': file_dates, 
+            'doc_type': file_types, 
+            'file_context': file_contexts,
+            'file_info': [pd.NA] * len(files)
+        })
+        file_metadata.to_csv(save_path / 'paper_info.csv', index=True)
+        
+    else:
+        file_metadata = pd.read_csv(save_path / 'paper_info.csv')
+        print(f"Loaded {len(file_metadata)} files from {save_path / 'paper_info.csv'}")
+        print(f"Remaining {file_metadata['file_info'].isna().sum()} files to process")
 
-    parsed_results = [result.result() for result in tqdm(results, desc="Parsing results")]
-    file_contents = pd.DataFrame(parsed_results, columns=['file_path', 'doc_type', 'info'])
-    file_info = pd.merge(file_metadata, file_contents, on='file_path')
+    return file_metadata
 
-    save_path = ROOT_PATH / doc_path.name
-    save_path.mkdir(exist_ok=True)
-    file_info.to_csv(save_path / 'paper_info.csv', index=True)
+def main(args):
+    doc_path = args.doc_path
+    save_window = args.save_window
+    n_pages_to_analyze = args.n_pages_to_analyze
+    n_tokens_to_context = args.n_tokens_to_context
+
+    save_path = ROOT_PATH / doc_path.name 
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    file_metadata = prepare_file_metadata(
+        doc_path, 
+        save_path, 
+        n_pages_to_analyze=n_pages_to_analyze, 
+        n_tokens_to_context=n_tokens_to_context
+    )
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = []
+        for _, row in tqdm(file_metadata.iterrows(), total=len(file_metadata), desc="Processing files"):
+            file_idx, file_context = row['file_idx'], row['file_context']
+            if pd.isna(row['file_info']):
+                futures.append(executor.submit(get_file_info, file_idx, file_context))
+            
+        for i, future in tqdm(enumerate(concurrent.futures.as_completed(futures)), total=len(futures), desc="Processing results"):
+            file_idx, info = future.result()
+            file_metadata.at[file_idx, 'file_info'] = info
+            if i % save_window == 0:
+                file_metadata.to_csv(save_path / 'paper_info.csv', index=True)
+        
+    file_metadata.to_csv(save_path / 'paper_info.csv', index=True)
+    print(f"Saved {len(file_metadata)} files to {save_path / 'paper_info.csv'}")
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--doc_path', type=Path, default=DEFAULT_DOC_PATH)
+    argparser.add_argument('--save_window', type=int, default=10)
+    argparser.add_argument('--n_pages_to_analyze', type=int, default=5)
+    argparser.add_argument('--n_tokens_to_context', type=int, default=100)
     args = argparser.parse_args()
-    main(args.doc_path)
+    main(args)
